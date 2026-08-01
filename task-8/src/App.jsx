64c9faa85c9fa1176676198f42b0a2bb82f6ba01
siddhartha220507 <singhsiddhartha953@gmail.com>
2026-08-01 20:03:56 +0530

    completed-task-8

--- task-8/src/App.jsx
+++ task-8/src/App.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
-
+import React, { useActionState, useCallback, useState } from 'react'
+import Card from './card'
 const App = () => {
+    
+  const [userName, setUserName] = useState('')
+  const [userRole, setUserRole] = useState('')
+  const [imageURL, setImageURL] = useState('')
+
+  const [allUsers, setAllUsers] = useState([])
+
+    const submitHandler = (e) => {
+      e.preventDefault()
+      setAllUsers([...allUsers, { userName, userRole, imageURL }])
+
+      setUserName('')
+      setUserRole('')
+      setImageURL('')
+
+    }
+
+     const deleteHandler = (idx) => {
+    const copyUsers = [...allUsers]
+
+    copyUsers.splice(idx, 1)
+
+    setAllUsers(copyUsers)
+  }
+  
+
   return (
-    <div className='flex p-4 gap-4 flex-wrap'>
-      <input required className='text-2xl p-2 bg-gray-50 font-black border-2 rounded-2xl border-b-gray-500 lg:w-[48%]' placeholder='Enter Name' type="text" />
-      <input className='text-2xl p-2 bg-gray-50 font-black border-2 rounded-2xl border-b-gray-500 lg:w-[48%]' placeholder='Enter Role' type="text" />
-      <input className='text-2xl p-2 bg-gray-50 font-black border-2 rounded-2xl border-b-gray-500 lg:w-[48%]' placeholder='Image Url' type="text" />
-      <button className='text-2xl p-2 bg-emerald-600 font-black rounded-2xl lg:w-[48%] cursor-pointer' >Submit</button>
+    <div className='p-7'>
+      <form onSubmit={(e) => {
+        submitHandler(e)
+      }} className='px-2 py-2 flex gap-5 flex-wrap'>
+
+      <input value={userName} onChange={(e)=>{
+        setUserName(e.target.value)
+      }} className='text-2xl p-2 bg-gray-50 font-black border-2 rounded-2xl border-b-gray-500 lg:w-[48%]' placeholder='userName' type="text" />
+      <input value={userRole} onChange={(e)=>{
+        setUserRole(e.target.value)
+      }} className='text-2xl p-2 bg-gray-50 font-black border-2 rounded-2xl border-b-gray-500 lg:w-[48%]' placeholder='userRole' type="text" />
+      <input value={imageURL} onChange={(e)=>{
+        setImageURL(e.target.value)
+      }} className='text-2xl p-2 bg-gray-50 font-black border-2 rounded-2xl border-b-gray-500 lg:w-[48%]' placeholder='imageUrl' type="text" />
+      <button className='text-2xl p-2 bg-emerald-600 font-black rounded-2xl lg:w-[48%] active:scale-95 cursor-pointer' >Submit</button></form>
+      <div className='px-4 py-10 gap-4 flex flex-wrap'>
+
+        {allUsers.map(function (elem, idx) {
+          return <Card idx={idx} elem={elem} deleteHandler={deleteHandler} />
+        })}
+
+      </div>
     </div>
   )
 }
